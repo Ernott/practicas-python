42f3b6031cca18e65a013e9c21d26fcb346c302e
Ernott <thegamer.58@hotmail.com>
2025-04-28 14:17:14 -0400

    Realizando p_s_3.py

---
+++ python_basico/clase3a/p_s_3.py
@@ -0,0 +1,11 @@
+# Descripción: encuentra el segundo número más grande de una lista.
+
+lista = [1,2,3,4,5,6,8,7,9]
+
+# Encuentra el numero mas grande
+
+for num in lista:
+    numero1 = max(lista) - 1
+print(numero1)
+
+# Encuentra el segundo numero mas grande
